c3c038687e370ba6d8f80fa71d185e6ba271b11b
Maurizio La Rocca <mrz.larocca@gmail.com>
2020-12-05 04:09:24 +0100

    GREEN per test findByIdSuccess

--- src/main/java/dev/frizio/web/demo/services/UserServiceImpl.java
+++ src/main/java/dev/frizio/web/demo/services/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package dev.frizio.web.demo.services;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import dev.frizio.web.demo.converters.UserConverter;
 import dev.frizio.web.demo.dtos.UserDTO;
+import dev.frizio.web.demo.entities.User;
 import dev.frizio.web.demo.repositories.UserRepository;
 
 @Service
@@ -22,6 +25,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO findById(Long id) {
-        return null;
+        User user = userRepository.findById(id).get();
+        return userConverter.userToUserDTO(user);
     }
 }
