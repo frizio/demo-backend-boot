17d51847837c61bcb1b0e62913ed82cdbe552aa3
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-12-07 16:03:44 +0100

    Convert indentation to spaces

--- src/test/java/dev/frizio/web/demo/DemoBackendBootApplicationTests.java
+++ src/test/java/dev/frizio/web/demo/DemoBackendBootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoBackendBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
