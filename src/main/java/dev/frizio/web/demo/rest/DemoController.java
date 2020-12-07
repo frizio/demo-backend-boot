3ef274738c402e2ce2498a5f0f3fcc78eeb3fb41
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-12-07 17:57:01 +0100

    Read custom application properties

--- src/main/java/dev/frizio/web/demo/rest/DemoController.java
+++ src/main/java/dev/frizio/web/demo/rest/DemoController.java
@@ -2,17 +2,22 @@ package dev.frizio.web.demo.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     
+    @Value("${team.developer.name}")
+    private String devName;
+
     @GetMapping("/demo")
     public String demo() {
         String out = "";
         out+=  "Time on server is " + LocalDateTime.now() + "\n";
         out += "Java version on the server machine: " + System.getProperty("java.version") + "\n";
+        out += "Developer: " + devName;
         return out;
     }
     
